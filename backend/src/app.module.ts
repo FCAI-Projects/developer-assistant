f21973aa38e327266c6d7db9a84560cf55f85308
Ezzdin Atef <ezzdin1125@gmail.com>
2022-03-22 00:40:15 +0200

    .

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-// mongodb://localhost:27017/developer-assistant
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/developer-assistant'),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
