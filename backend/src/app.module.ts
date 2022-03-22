0937218086e11dc876083594a05f9b07043adf38
Ezzdin Atef <ezzdin1125@gmail.com>
2022-03-22 12:11:36 +0200

    generate user module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 // mongodb://localhost:27017/developer-assistant
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/developer-assistant'),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
