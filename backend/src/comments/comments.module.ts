291bb1a552dce36bc752a077c44f879eabfe733d
AbdElrhman Ahmed <abdelrhman8889@gmail.com>
2022-03-26 00:50:41 +0200

    just pusch task but didn't test it

--- backend/src/comments/comments.module.ts
+++ backend/src/comments/comments.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsResolver } from './comments.resolver';
+import { MongooseModule, Schema } from '@nestjs/mongoose';
+import { Comment, CommentSchema } from './entities/comment.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Comment.name,
+        schema: CommentSchema,
+      },
+    ]),
+  ],
   providers: [CommentsResolver, CommentsService]
 })
 export class CommentsModule {}
--- backend/src/comments/dto/create-comment.input.ts
+++ backend/src/comments/dto/create-comment.input.ts
@@ -1,7 +1,13 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType,  Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateCommentInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String, { description: 'Comment ID' })
+  commentId: String;
+
+  @Field (() => String, {description: 'User ID'})
+  userID : String;
+
+  @Field (()=> String, {description: 'Comment Content'})
+  commentContent: String;
 }
