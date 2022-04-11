471a23e74eb0624221f4882fa02af1cc57938ce6
Ezzdin Atef <ezzdin1125@gmail.com>
2022-04-11 14:15:58 +0200

    ss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,22 @@
+import { Routes, Route, Outlet } from "react-router-dom";
 import React from "react";
+import { ProjectCard } from "./components/ProjectCard";
 
 export const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/">
+          <Route index />
+          <Route path="login" />
+          <Route path="register" />
+        </Route>
+        <Route path="/app">
+          <Route index />
+          <Route path="login" />
+          <Route path="register" />
+        </Route>
+      </Routes>
+    </div>
+  );
 };
--- frontend/src/components/ProjectCard.tsx
+++ frontend/src/components/ProjectCard.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 
-export const ProjectCard: React.FC = () => {
-  return <div>ProjectCard</div>;
+interface ProjectCardProps {
+  name: string;
+  description: string;
+  link: string;
+}
+
+export const ProjectCard: React.FC<ProjectCardProps> = ({ name, description, link }) => {
+  return (
+    <div className="w-3/12">
+      <a href={link} className="block text-center">
+        <div className="overflow-hidden rounded-md shadow-lg hover:shadow-xl">
+          <div className="px-6 py-6">
+            <div className="mb-2 text-xl font-bold">{name}</div>
+            <p className="text-base text-gray-700">{description}</p>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
 };
