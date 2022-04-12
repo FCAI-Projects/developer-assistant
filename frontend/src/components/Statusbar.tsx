6d5f3547c7e9522107b8575b6879c0821382b4ee
Ezzdin Atef <ezzdin1125@gmail.com>
2022-04-12 18:17:38 +0200

    finish my f***** task

---
+++ frontend/src/components/Statusbar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Statusbar: React.FC = () => {
+  return <div className="absolute bottom-0 w-full border-t bg-slate-50 py-1 px-5 text-sm ">Statusbar</div>;
+};
