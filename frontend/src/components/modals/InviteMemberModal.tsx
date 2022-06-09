764cbbd38f440e62a4e1fb52cd399fedff979eaa
AbdElrhman Ahmed <abdelrhman8889@gmail.com>
2022-06-09 18:41:19 +0200

    done

--- frontend/src/components/modals/InviteMemberModal.tsx
+++ frontend/src/components/modals/InviteMemberModal.tsx
@@ -7,59 +7,64 @@ import { Modal } from "./Base";
 import { useMutation } from "@apollo/client";
 import { InvitedMemberDocument, InviteMemberDocument, useRolesQuery } from "../../graphql/generated/graphql";
 import { CustomSelect, Input, Label } from "../forms";
-import { FaUserPlus } from "react-icons/fa";
+import { FaPlus, FaUserPlus } from "react-icons/fa";
 import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 // TODO: Use the right query to save to database
-
+interface formikProps {
+  member: string;
+  role: { id: string; name: string }; 
+}
 export const InviteMemberModal: React.FC = () => {
   const params = useParams();
   const [rolesOptions, setRolesOptions] = useState<any>([]);
   const { data: Roles } = useRolesQuery({ variables: { project: params.id as string } });
-  const [InviteMember, { loading }] = useMutation(InviteMemberDocument, { refetchQueries: [{query: InvitedMemberDocument , variables:{project: params.id}}],});
+  const [InviteMember, { loading }] = useMutation(InviteMemberDocument);
   const [isOpen, toggleModal] = useToggleModal();
-  // console.log(Roles);
-  const formik = useFormik({
+  const formik = useFormik<formikProps>({
     initialValues: {
-      memberEmail: "",
-      role: "",
+      member: "",
+      role: { id: "", name: "role" }
     },
     validationSchema: Yup.object({
       member: Yup.string().email().required("Required"),
-      role: Yup.string().required("Required"),
+      role: Yup.object().required("Required"),
     }),
     onSubmit: async (values) => {
       try {
-        await InviteMember({ 
+          await InviteMember({ 
           variables: {
             inviteMemberInput: {
-              memberEmail: values.memberEmail,
-              role: values.role,
               project: params.id,
+              user: values.member,
+              role: values.role.id,
+              
             },
           },
         });
+        toast.success("Member has been invited !");
+        toggleModal();
+        
       } catch (error) {
         console.log(error);
       }
     },
   });
- useEffect (() => {
-   if (Roles) 
-    setRolesOptions(
-      Roles.roles.map((role:any) => ({
-         id : role.id,
-         name: role.name,
-         })));
- } , [Roles]);
-  
-
+  useEffect (() => {
+    if (Roles) 
+     setRolesOptions(
+       Roles.roles.map((role:any) => ({
+          id : role.id,
+          name: role.name,
+          })));
+  } , [Roles]);
   return (
     <>
       <Button lightBlue className="flex items-center gap-2 px-3 py-2 text-xs" onClick={toggleModal}>
         <FaUserPlus /> Invite Member
       </Button>
-      <Modal title="Invite Member" isOpen={isOpen} handleClose={toggleModal}>
+      <Modal title="Create New Project" isOpen={isOpen} handleClose={toggleModal}>
         <div className="my-5">
           <form className="flex flex-col gap-4">
             <div className="w-full">
@@ -69,15 +74,16 @@ export const InviteMemberModal: React.FC = () => {
             <div className="w-full">
               <Label htmlFor="role">Role</Label>
               <CustomSelect
-                options={rolesOptions}
-                value={formik.values.role}
-                onChange={(e) => {
-                  formik.setFieldValue("role", e);
-                  
-                }}
-                label="name"
-                id="id"
-              />
+              
+              options={rolesOptions}
+              value={formik.values.role}
+              onChange={(e) => {
+                formik.setFieldValue("role", e);
+                
+              }}
+              label="name"
+              id="id"
+            />
             </div>
           </form>
         </div>
