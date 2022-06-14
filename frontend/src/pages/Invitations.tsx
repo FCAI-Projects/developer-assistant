d4a3acbd064628cb4e6ec058829852c5b028dcbd
Mahmoud Sobhy <mahmoudsobhy23hf@gmail.com>
2022-06-14 17:56:18 +0200

    handel invitations

--- frontend/src/pages/Invitations.tsx
+++ frontend/src/pages/Invitations.tsx
@@ -1,36 +1,67 @@
-
 import { useMutation } from "@apollo/client";
 import { Button } from "../components/Button";
-
-
 import { UpdateMemberDocument, useInvitationsQuery } from "../graphql/generated/graphql";
 
 export const Invitations: React.FC = () => {
   const { data, error, loading } = useInvitationsQuery();
-  //  const { updateMember } = useMutation (UpdateMemberDocument);
-  
-  
+  const [updateMember, { loading: loadingUpdate} ] = useMutation (UpdateMemberDocument, {
+    refetchQueries: ["Invitations"]
+  });
+
+  const handleAccept = (id: string) => {
+    updateMember({
+      variables: {  
+        updateMemberId: id,
+        updateMemberInput: {
+          status: "joined" 
+        }
+      }
+    });
+  }
+  const handleDecline = (id: string) => {
+    updateMember({
+      variables: {  
+        updateMemberId: id,
+        updateMemberInput: {
+          status: "declined" 
+        }
+      }
+    });
+  }
+
   return (
     <div className="container mx-auto my-5">
       <h2 className="text-2xl font-bold">Invitations</h2>
       <div className="flex flex-col gap-3">
-        <div className="flex items-center justify-between border-b p-3">
-       {data && data.invitations.map( (Invitations) => {
-        return <div>
-           <div>
-            <h4 className="text-lg">{Invitations.project.name}</h4>
-          </div>
-          
-          <Button lightGreen>Accept</Button>
-            <Button lightRed >Decline</Button>
-           </div>
-       })}
-       
-        
-        </div>
+        {data && data.invitations.map((invitation) => {
+          return (
+            <div 
+              key={invitation.id}
+              className="flex items-center justify-between border-b p-3"
+            >
+              <div>
+                <h4 className="text-lg">{invitation.user.fname+" "+invitation.user.lname}</h4>
+              </div>
+              <div className="flex gap-2">
+                <Button 
+                  lightGreen
+                  onClick={() => handleAccept(invitation.id)}
+                  loading={loadingUpdate}
+                >
+                  Accept
+                </Button>
+                <Button 
+                  lightRed
+                  onClick={() => handleDecline(invitation.id)}
+                  loading={loadingUpdate}
+                >
+                  Decline
+                </Button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 };
-
-
